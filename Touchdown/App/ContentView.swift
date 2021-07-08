2c5997ae4ddcace5cdd6eaab736313bd72c26cf3
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-07-08 15:04:41 +0200

    Custom Navigation Bar with logo

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -16,8 +16,23 @@ struct ContentView: View {
     // MARK: - Body
     
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            } // VStack
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } // ZStack
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
